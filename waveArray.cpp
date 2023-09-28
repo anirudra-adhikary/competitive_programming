2c39f8f2e6294da8ab60ea4156cfe5c9dc1511ea
anirudra-adhikary <anirudraadhikary123@gmail.com>
2023-09-28 20:53:54 +0530

    Wave Array Sorting Problem from gfg

---
+++ waveArray.cpp
@@ -0,0 +1,93 @@
+/*
+This is a problem from "Problem of the day" of GeeksForGeeks. Date - 28 sep, 2023
+
+Given a sorted array arr[] of distinct integers. Sort the array into a wave-like array(In Place).
+In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5].....
+
+If there are multiple solutions, find the lexicographically smallest one.
+
+Note:The given array is sorted in ascending order, and you don't need to return anything to make changes in the original array itself.
+
+Example 1:
+
+Input:
+n = 5
+arr[] = {1,2,3,4,5}
+Output: 2 1 4 3 5
+Explanation: Array elements after 
+sorting it in wave form are 
+2 1 4 3 5.
+Example 2:
+
+Input:
+n = 6
+arr[] = {2,4,7,8,9,10}
+Output: 4 2 8 7 10 9
+Explanation: Array elements after 
+sorting it in wave form are 
+4 2 8 7 10 9.
+Your Task:
+The task is to complete the function convertToWave(), which converts the given array to a wave array.
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ n ≤ 10^6
+0 ≤ arr[i] ≤10^7 
+
+Company Tags : Paytm Flipkart Amazon Microsoft FactSet GoldmanSachs Google
+Topic Tags: Arrays Sorting Data Structures Algorithms*/
+
+
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+// #include <algorithm>
+
+
+// } Driver Code Ends
+class Solution{
+    
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(int n, vector<int>& arr){
+        
+        for(int i=0;i<n-1;i+=2)
+        {
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t; //Input testcases
+    while(t--) //While testcases exist
+    {
+        cin>>n; //input size of array
+        vector<int> a(n); //declare vector of size n
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //input elements of array
+        sort(a.begin(),a.end());
+        Solution ob;
+        ob.convertToWave(n, a);
+
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<" "; //print array
+            
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
